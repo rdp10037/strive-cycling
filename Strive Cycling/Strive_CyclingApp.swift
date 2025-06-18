0687d300f18cdd1e1106f4b52173e3d99dfc4a61
rdp10037 <168250338+rdp10037@users.noreply.github.com>
2025-06-18 18:01:18 -0400

    created xcode project

---
+++ Strive Cycling/Strive_CyclingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Strive_CyclingApp.swift
+//  Strive Cycling
+//
+//  Created by Rob Pee on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Strive_CyclingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Strive CyclingTests/Strive_CyclingTests.swift
@@ -0,0 +1,17 @@
+//
+//  Strive_CyclingTests.swift
+//  Strive CyclingTests
+//
+//  Created by Rob Pee on 6/18/25.
+//
+
+import Testing
+@testable import Strive_Cycling
+
+struct Strive_CyclingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
